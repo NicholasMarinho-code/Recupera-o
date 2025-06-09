b4d6afa5c66952ff3ddf799de554ce74a3b0b54f
NicholasMarinho-code <nearmarinhos@gmail.com>
2025-06-09 13:54:15 -0300

    Add files via upload

---
+++ Exercicio15.java
@@ -0,0 +1,37 @@
+package exercicio15;
+
+import javax.swing.JOptionPane;
+
+public class Exercicio15 {
+    public static void main(String[] args) {
+        double saldo = Double.parseDouble(JOptionPane.showInputDialog("Quanto tem de saldo:"));
+        String opcao = JOptionPane.showInputDialog("Digite uma opção entre: saque, depósito ou saldo:");
+        
+        switch (opcao.toLowerCase()) {
+            case "saque":
+                double saque = Double.parseDouble(JOptionPane.showInputDialog("Quanto você quer sacar:"));
+                if (saldo == 0 || saque > saldo) {
+                    JOptionPane.showMessageDialog(null, "Você não pode sacar esse valor.");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Você sacou: " + saque);
+                }
+                break;
+
+            case "depósito":
+                double deposito = Double.parseDouble(JOptionPane.showInputDialog("Quanto você quer depositar:"));
+                JOptionPane.showMessageDialog(null, "Você depositou: " + deposito);
+                 JOptionPane.showMessageDialog(null, "Seu saldo agora é de: " + (deposito + saldo));
+                break;
+
+            case "saldo":
+                JOptionPane.showMessageDialog(null, "Seu saldo é de: " + saldo);
+                break;
+
+            default:
+                JOptionPane.showMessageDialog(null, "Opção inválida!");
+                break;
+        }
+        
+    }
+    
+}
