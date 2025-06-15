4b9248d54da837fcbc4429bbb6eddb365d399fc6
NicholasMarinho-code <nearmarinhos@gmail.com>
2025-06-15 11:09:30 -0300

    Update Exercicio23.java

--- Exercicio23.java
+++ Exercicio23.java
@@ -1,24 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package exercicio23;
-
-/**
- *
- * @author Administrador
- */
 public class Exercicio23 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-    
+       int soma = 0;
        for (int i = 1; i<=50; i++ ) {
-           if( i%2 == 0) { System.out.println("o numero " + i + " e par");
+           if( i%2 == 0) { soma = soma + i;
        } 
        }
+       JOptionPane.showMessageDialog(null, "A soma de todos os números pares é de: " + soma);
     }
 }
 
